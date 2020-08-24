2cdb9825360a7ad0ee39447d467850ab3d8e581f
mark.jang <>
2020-08-24 19:27:56 +0900

    Flag (Html.FROM_HTML_MODE_LEGACY) 추가

--- app/src/main/java/io/hwjang/app/weather/util/BindingAdapters.kt
+++ app/src/main/java/io/hwjang/app/weather/util/BindingAdapters.kt
@@ -1,8 +1,10 @@
 package io.hwjang.app.weather.util
 
+import android.os.Build
 import android.text.Html
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("loading")
@@ -10,8 +12,9 @@ fun loading(view: View, visible: Boolean) {
     view.visibility = if (visible) View.VISIBLE else View.GONE
 }
 
+@RequiresApi(Build.VERSION_CODES.N)
 @BindingAdapter("toHtml")
 fun toHtml(tv: TextView, text: String) {
-    tv.text = Html.fromHtml(text)
+    tv.text = Html.fromHtml(text,Html.FROM_HTML_MODE_LEGACY)
 }
 
